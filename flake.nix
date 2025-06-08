b9b69589ed8d937d5c16c407a54bea02d06627d7
André Lima <me@limwa.pt>
2025-06-08 22:34:58 +0100

    feat: add basic template

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A flake for simplifying repetitive tasks in Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:limwa/nix-flake-utils";
+
+    # Needed for shell.nix
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.mkFlakeWith {
+      forEachSystem = system: {
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      };
+    } {
+      formatter = {pkgs}: pkgs.alejandra;
+
+      templates = {
+        basic = utils.lib.mkTemplate {
+          path = ./templates/basic;
+        };
+      };
+    };
+}
---
+++ templates/basic/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A basic flake for development with Nix and NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:limwa/nix-flake-utils";
+
+    # Needed for shell.nix
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.mkFlake {
+      forEachSystem = system: {
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      };
+    } {
+      formatter = {pkgs, ...}: pkgs.alejandra;
+
+      devShell = {pkgs, ...}:
+        pkgs.mkShell {
+          shellHook = ''
+            echo "Your development environment is ready!"
+            echo "To edit this message, modify the shellHook in flake.nix"
+          '';
+        };
+    };
+}
