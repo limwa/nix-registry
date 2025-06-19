ac294afb59a75c759725459d9d55b82f06545f46
André Lima <me@limwa.pt>
2025-06-19 02:05:20 +0100

    chore(template-deno): use correct PR branch

--- templates/deno/flake.nix
+++ templates/deno/flake.nix
@@ -2,7 +2,7 @@
   description = "A basic flake for Deno development with Nix and NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=pull/417619/merge";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=pull/416177/merge";
     utils.url = "github:limwa/nix-flake-utils";
 
     # Needed for shell.nix
