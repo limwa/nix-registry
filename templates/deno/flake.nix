f6430fe2a22aeb581643ec5583f90577e005ee96
André Lima <me@limwa.pt>
2025-06-20 19:32:18 +0100

    chore(template-deno): use nixos-unstable branch

--- templates/deno/flake.nix
+++ templates/deno/flake.nix
@@ -2,7 +2,7 @@
   description = "A basic flake for Deno development with Nix and NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:limwa/nix-flake-utils";
 
     # Needed for shell.nix
