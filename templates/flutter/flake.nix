c276e969e4cd670c1692286c0b16de335a943013
André Lima <me@limwa.pt>
2025-09-05 00:11:04 +0100

    chore(template-flutter): use fix in nixpkgs

--- templates/flutter/flake.nix
+++ templates/flutter/flake.nix
@@ -2,7 +2,7 @@
   description = "A basic flake for Flutter development with Nix and NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/pull/435027/merge";
     utils.url = "github:limwa/nix-flake-utils";
 
     # Needed for shell.nix
