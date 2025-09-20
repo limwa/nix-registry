2dca7368252d07f316c42a1bc031417a27eeaeff
André Lima <me@limwa.pt>
2025-09-20 20:10:12 +0100

    refactor(template-flutter): reduce closure size by separating emulator

--- overlays/android/internal/wrap-emulator-with.nix
+++ overlays/android/internal/wrap-emulator-with.nix
@@ -4,8 +4,14 @@
   runCommandNoCC,
   # Packages
   makeWrapper,
-}: {useHardwareGraphics ? true}: emulator: let
-  base = emulator.override (oldAttrs: {
+}: {
+  meta ? {},
+  useHardwareGraphics ? true,
+}: emulator: let
+  unwrapped = emulator
+    .override (oldAttrs: {
+    name = "${oldAttrs.name}-unwrapped";
+
     configOptions =
       (oldAttrs.configOptions or {})
       // lib.optionalAttrs useHardwareGraphics {
@@ -13,19 +19,34 @@
         "hw.gpu.mode" = "host";
       };
   });
-in
+
+  unwrappedMeta = lib.getAttrs ["description" "homepage"] unwrapped.meta;
+
   # To make the emulator pure, all variables related to the emulator need to be
   # taken care of.
   # https://developer.android.com/tools/variables
-  runCommandNoCC "${base.name}-wrapped" {
-    nativeBuildInputs = [makeWrapper];
-    meta.mainProgram = base.meta.mainProgram or "emulator";
-  } ''
-    mkdir -p $out/bin
-    makeWrapper "${lib.getExe base}" "$out/bin/${emulator.meta.mainProgram}" \
-      --unset ANDROID_HOME \
-      --unset ANDROID_USER_HOME \
-      --unset ANDROID_EMULATOR_HOME \
-      --unset ANDROID_AVD_HOME \
-      --set ANDROID_EMULATOR_USE_SYSTEM_LIBS 1
-  ''
+  wrapper =
+    runCommandNoCC "${lib.removeSuffix "-unwrapped" unwrapped.name}" {
+      nativeBuildInputs = [makeWrapper];
+
+      meta =
+        unwrappedMeta
+        // meta
+        // {
+          mainProgram = unwrapped.meta.mainProgram or "emulator";
+        };
+
+      passthru = {
+        inherit unwrapped;
+      };
+    } ''
+      mkdir -p $out/bin
+      makeWrapper "${lib.getExe unwrapped}" "$out/bin/${wrapper.meta.mainProgram}" \
+        --unset ANDROID_HOME \
+        --unset ANDROID_USER_HOME \
+        --unset ANDROID_EMULATOR_HOME \
+        --unset ANDROID_AVD_HOME \
+        --set ANDROID_EMULATOR_USE_SYSTEM_LIBS 1
+    '';
+in
+  wrapper
--- templates/flutter/flake.nix
+++ templates/flutter/flake.nix
@@ -5,6 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:limwa/nix-flake-utils";
 
+    # For hardware-accelerated Android emulator on NixOS
+    limwa.url = "github:limwa/nix-registry";
+
     # Needed for shell.nix
     flake-compat.url = "github:edolstra/flake-compat";
   };
@@ -13,6 +16,7 @@
     self,
     nixpkgs,
     utils,
+    limwa,
     ...
   }:
     utils.lib.mkFlakeWith {
@@ -21,17 +25,21 @@
 
         pkgs = import nixpkgs {
           inherit system;
+
           config = {
             allowUnfree = true;
             android_sdk.accept_license = true;
           };
+
+          overlays = [
+            # For hardware-accelerated Android emulator on NixOS
+            limwa.overlays.android
+          ];
         };
 
         # Reuse the same Android SDK, JDK and Flutter versions across all derivations
         androidComposition = pkgs.androidenv.composeAndroidPackages {
-          includeEmulator = "if-supported";
           includeNDK = "if-supported";
-          includeSystemImages = "if-supported";
 
           buildToolsVersions = ["34.0.0"];
           cmakeVersions = ["3.22.1"];
@@ -60,8 +68,6 @@
             meta.description = "A development shell with Flutter and an Android SDK installation";
 
             env = {
-              EMULATOR_NAME = "emulator-5554";
-
               # Android environment variables
               ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
               GRADLE_OPTS = let
@@ -70,42 +76,61 @@
 
               # Java environment variables
               JAVA_HOME = "${jdk}";
-
-              # Needed for graphics hardware acceleration in the emulator
-              LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [libGL];
             };
 
             packages = [
               androidComposition.androidsdk
               flutter
               jdk
-
-              (pkgs.writeShellApplication {
-                name = "emulator-setup";
-
-                text = ''
-                  avdmanager delete avd -n "${env.EMULATOR_NAME}" 2>/dev/null || true
-                  avdmanager create avd -n "${env.EMULATOR_NAME}" -k "system-images;android-35;google_apis;x86_64" -d "pixel_9_pro_xl"
-
-                  # Enable GPU acceleration
-                  {
-                    echo "hw.gpu.enabled=yes"
-                    echo "hw.gpu.mode=host"
-                  } >> "$HOME/.android/avd/${env.EMULATOR_NAME}.avd/config.ini"
-
-                  avdmanager list avd
-                '';
-              })
-
-              (pkgs.writeShellApplication {
-                name = "emulator-launch";
-
-                text = ''
-                  flutter emulators --launch "${env.EMULATOR_NAME}"
-                '';
-              })
             ];
           };
+      };
+
+      packages = utils.lib.invokeAttrs {
+        emulator = {
+          pkgs,
+          androidComposition,
+          ...
+        }: let
+          # By default, the emulator uses the platform version of the last platform
+          # specified in the Android SDK
+          #
+          # If you want to use a different platform version, you can change the
+          # `platformVersion` variable below.
+          platformVersion = pkgs.lib.getVersion (pkgs.lib.last androidComposition.platforms);
+        in
+          pkgs.limwa.android.wrapEmulatorWith {
+            meta.description = "An Android emulator, running SDK version ${platformVersion}";
+            useHardwareGraphics = true;
+          } (
+            pkgs.androidenv.emulateApp {
+              name = "my-emulator";
+              deviceName = "my_emulator";
+
+              inherit platformVersion;
+              systemImageType = "google_apis_playstore";
+
+              # The emulator uses the instruction set of the host system
+              # to avoid emulating alien architectures (e.g. arm64 on x86_64)
+              #
+              # If you want to emulate a specific architecture, you can change
+              # the ABI version below.
+              abiVersion =
+                if pkgs.stdenv.isAarch32
+                then "armeabi-v7a"
+                else if pkgs.stdenv.isAarch64
+                then "arm64-v8a"
+                else if pkgs.stdenv.isx86_32
+                then "x86"
+                else if pkgs.stdenv.isx86_64
+                then "x86_64"
+                else throw "Please specify an ABI version for your system";
+
+              # Specify user home to speed up boot times and avoid creating
+              # a lot of avd instances taking up disk space
+              androidUserHome = "\$HOME/.android";
+            }
+          );
       };
     };
 }
