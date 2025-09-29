abf93553f492f1c42fdf012b31ddad31430b27de
André Lima <me@limwa.pt>
2025-09-29 20:46:36 +0100

    fix: gpu acceleration on android

--- overlays/android/internal/wrap-emulator-with.nix
+++ overlays/android/internal/wrap-emulator-with.nix
@@ -4,20 +4,23 @@
   runCommandNoCC,
   # Packages
   makeWrapper,
+  libGL,
+  vulkan-loader,
 }: {
   meta ? {},
   useHardwareGraphics ? true,
 }: emulator: let
-  unwrapped = emulator
-    .override (oldAttrs: {
+  unwrapped = emulator.override (oldAttrs: {
     name = "${oldAttrs.name}-unwrapped";
 
     configOptions =
       (oldAttrs.configOptions or {})
-      // lib.optionalAttrs useHardwareGraphics {
-        "hw.gpu.enabled" = "yes";
-        "hw.gpu.mode" = "host";
-      };
+      // (
+        lib.optionalAttrs useHardwareGraphics {
+          "hw.gpu.enabled" = "yes";
+          "hw.gpu.mode" = "host";
+        }
+      );
   });
 
   unwrappedMeta = lib.getAttrs ["description" "homepage"] unwrapped.meta;
@@ -39,14 +42,21 @@
       passthru = {
         inherit unwrapped;
       };
-    } ''
-      mkdir -p $out/bin
-      makeWrapper "${lib.getExe unwrapped}" "$out/bin/${wrapper.meta.mainProgram}" \
-        --unset ANDROID_HOME \
-        --unset ANDROID_USER_HOME \
-        --unset ANDROID_EMULATOR_HOME \
-        --unset ANDROID_AVD_HOME \
-        --set ANDROID_EMULATOR_USE_SYSTEM_LIBS 1
-    '';
+    } (
+      ''
+        mkdir -p $out/bin
+        makeWrapper "${lib.getExe unwrapped}" "$out/bin/${wrapper.meta.mainProgram}" \
+          --unset ANDROID_HOME \
+          --unset ANDROID_USER_HOME \
+          --unset ANDROID_EMULATOR_HOME \
+          --unset ANDROID_AVD_HOME \
+          --set ANDROID_EMULATOR_USE_SYSTEM_LIBS 0
+      ''
+      + lib.optionalString useHardwareGraphics ''
+        wrapProgram "$out/bin/${wrapper.meta.mainProgram}" \
+          --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [libGL]}" \
+          --set ANDROID_EMU_VK_LOADER_PATH "${vulkan-loader}/lib/libvulkan.so"
+      ''
+    );
 in
   wrapper
