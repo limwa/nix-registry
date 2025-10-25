701235ba525b9957dcbeb909f29fde0feb5700a3
André Lima <me@limwa.pt>
2025-10-25 18:34:30 +0100

    feat(templates): add typst template

---
+++ templates/typst/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A basic flake for Typst writting with Nix and NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:limwa/nix-flake-utils";
+
+    # Needed for shell.nix
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.mkFlakeWith {
+      forEachSystem = system: {
+        outputs = utils.lib.forSystem self system;
+
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      };
+    } {
+      formatter = {pkgs, ...}: pkgs.alejandra;
+
+      devShells = utils.lib.invokeAttrs {
+        default = {outputs, ...}: outputs.devShells.typst;
+
+        # Typst development shell
+        typst = {pkgs, ...}:
+          pkgs.mkShellNoCC {
+            meta.description = "A development shell with Typst";
+
+            packages = with pkgs; [
+              typst
+            ];
+          };
+      };
+    };
+}
